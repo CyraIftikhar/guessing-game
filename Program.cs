e3b61bc45af1c9dc7e96d943e895070c4c25015a
cyraiftikhar <CyraIftikhar@hotmail.com>
2023-04-13 00:41:24 +0100

    guess can be upper or lower case to win

--- Program.cs
+++ Program.cs
@@ -1,21 +1,23 @@
-﻿string secretWord = "Princey"; // my cat 
-string guess = "";
+﻿string secretWord = "strawberry";
+string guess = " ";
 int guessCount = 0;
 int guessLimit = 10;
 bool outOfGuesses = false;
 
-while (guess != secretWord && !outOfGuesses)
+while (guess.ToLower() != secretWord.ToLower() && !outOfGuesses)
 {
     if (guessCount < guessLimit)
     {
         Console.Write("Enter guess: ");
         guess = Console.ReadLine();
+        guess = guess.ToLower();
+
         guessCount++;
 
         int numMatches = 0;
         for (int i = 0; i < guess.Length && i < secretWord.Length; i++)
         {
-            if (guess[i] == secretWord[i])
+            if (guess[i] == secretWord[i] || guess[i] == secretWord[i].ToString().ToLower() || guess[i] == secretWord[i].ToString().ToUpper())
             {
                 numMatches++;
             }
@@ -36,7 +38,7 @@ while (guess != secretWord && !outOfGuesses)
 
         if (guessCount == 3)
         {
-            Console.WriteLine("Hint: The first letter of the secret word is {0}.", secretWord[0]);
+            Console.WriteLine("Hint: It's a tasty red fruit.");
         }
     }
     else
@@ -55,6 +57,5 @@ else
 }
 
 Console.ReadLine();
-
 
 
