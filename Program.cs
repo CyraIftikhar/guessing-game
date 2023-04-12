60878f22afa88ff29729d85290955e0b51a0b9a9
cyraiftikhar <CyraIftikhar@hotmail.com>
2023-04-13 00:29:03 +0100

    added for loop and if statements for whether guess is close

--- Program.cs
+++ Program.cs
@@ -5,19 +5,43 @@ int guessLimit = 10;
 bool outOfGuesses = false;
 
 while (guess != secretWord && !outOfGuesses)
-        {
+{
     if (guessCount < guessLimit)
     {
         Console.Write("Enter guess: ");
         guess = Console.ReadLine();
         guessCount++;
+
+      
+        int numMatches = 0;
+        for (int i = 0; i < guess.Length && i < secretWord.Length; i++)
+        {
+            if (guess[i] == secretWord[i])
+            {
+                numMatches++;
+            }
+        }
+
+
+        if (numMatches == secretWord.Length)
+        {
+            Console.WriteLine("Congratulations, you guessed the word!");
+        }
+        else if (numMatches > 0)
+        {
+            Console.WriteLine("You're close! {0} letters match the secret word.", numMatches);
+        }
+        else
+        {
+            Console.WriteLine("Incorrect guess. Try again.");
+        }
     }
     else
     {
         outOfGuesses = true;
     }
-
 }
+
 if (outOfGuesses)
 {
     Console.Write("You Lose");
